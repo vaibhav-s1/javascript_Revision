ee50cbaaf11f8173c9948c80b138cc442ae482bd
vaibhav-s1 <vaibhavshimpi33@gmail.com>
2026-02-12 13:09:30 +0000

    Stings methods learned

---
+++ basics/string.js
@@ -0,0 +1,8 @@
+const gamename =new  String('cricket')
+console.log(gamename[0])
+
+console.log(gamename.charAt(4))
+console.log(gamename.indexOf('k'))
+
+const newString= gamename.substring(0,4)
+console.log(newString)
