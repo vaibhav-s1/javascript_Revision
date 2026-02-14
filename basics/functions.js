27135c9a0dd919ac96f69fe8d28e55cf7f2bc450
vaibhav-s1 <vaibhavshimpi33@gmail.com>
2026-02-14 12:52:20 +0000

    functions

---
+++ basics/functions.js
@@ -0,0 +1,11 @@
+// function add(number1,number2){
+//      console.log(number1+number2)
+// }
+// add(4,5)
+
+function add(number1,number2){
+    return number1+number2;
+}
+const result =add(34,5);
+
+console.log(`result of addition is: ${result}`);
