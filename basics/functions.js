81aa560c5b845d3d1ed986f7b2b17231f43f1f59
vaibhav-s1 <vaibhavshimpi33@gmail.com>
2026-02-15 17:13:57 +0000

    object learned

--- basics/functions.js
+++ basics/functions.js
@@ -9,3 +9,22 @@ function add(number1,number2){
 const result =add(34,5);
 
 console.log(`result of addition is: ${result}`);
+
+const arr=[3,4,5]
+
+console.log(arr[1])
+
+const nums=[];
+nums.push("name")
+nums.unshift('my')
+nums.push("is")
+nums.push("vaibhav")
+
+console.log(nums)
+
+const obj={ 
+    name:"vaibhav",
+    age:21,
+
+}
+console.log(obj.name)
